8d2d3b88aac53b027a7f64de1ea51822a78eda93
wimb0 <11030068+wimb0@users.noreply.github.com>
2024-09-09 19:04:01 +0000

    :art: Format Python code with psf/black

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def main(receiver_ip, receiver_user, receiver_pass, reset_unbound_stats: bool):
     logger.debug(f"Param reset_unbound_stats = {reset_unbound_stats}")
     unbound_stats = Collector.get_statistics(reset_unbound_stats)
     if unbound_stats is not None:
-        statistic_sent = Publisher.send_statistics(receiver_ip, receiver_user, receiver_pass, unbound_stats, publisher_cntnr)
+        statistic_sent = Publisher.send_statistics(
+            receiver_ip, receiver_user, receiver_pass, unbound_stats, publisher_cntnr
+        )
 
         if statistic_sent:
             logger.debug(f"Unbound statistics sent successful.")
@@ -25,23 +27,42 @@ def main(receiver_ip, receiver_user, receiver_pass, reset_unbound_stats: bool):
     else:
         logger.error(f"Unbound statistics gather failed. Check log for details.")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("receiver_ip", help="IP address of MQTT Broker")
     parser.add_argument("receiver_user", help="Username for MQTT Broker")
     parser.add_argument("receiver_pass", help="Password for MQTT Broker")
-    parser.add_argument("-nr", "--no-reset", action="store_false", dest="reset_unbound_stats",
-                        help="Do not reset unbound statistics")
-    parser.add_argument("--debug", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.WARNING,
-                        help="Set loglevel to DEBUG")
+    parser.add_argument(
+        "-nr",
+        "--no-reset",
+        action="store_false",
+        dest="reset_unbound_stats",
+        help="Do not reset unbound statistics",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.WARNING,
+        help="Set loglevel to DEBUG",
+    )
     args = parser.parse_args()
 
     logdir = "log"
     if not os.path.isdir(logdir):
         os.makedirs(logdir)
 
-    logging.basicConfig(level=args.loglevel, filename=f"{logdir}/unbound-stats.log",
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=args.loglevel,
+        filename=f"{logdir}/unbound-stats.log",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
-    main(args.receiver_ip, args.receiver_user, args.receiver_pass, args.reset_unbound_stats)
-
+    main(
+        args.receiver_ip,
+        args.receiver_user,
+        args.receiver_pass,
+        args.reset_unbound_stats,
+    )
--- send_statistics.py
+++ send_statistics.py
@@ -7,22 +7,46 @@ logger = logging.getLogger(__name__)
 
 
 class Publisher:
-
     @staticmethod
-    def send_statistics(receiver_ip: str, receiver_user: str, receiver_pass: str, stats: str, publisher_cntnr: str) -> bool:
-
+    def send_statistics(
+        receiver_ip: str,
+        receiver_user: str,
+        receiver_pass: str,
+        stats: str,
+        publisher_cntnr: str,
+    ) -> bool:
         __topic__ = f"unbound/stats/{get_hostname('lower')}"
 
         try:
-            mqttsend = subprocess.run((["mosquitto_pub", "-h", f"{receiver_ip}", "-u", f"{receiver_user}", "-P", f"{receiver_pass}","-q", "1", "-t", f"{__topic__}", "-m", f"{stats}"]),stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            mqttsend = subprocess.run(
+                (
+                    [
+                        "mosquitto_pub",
+                        "-h",
+                        f"{receiver_ip}",
+                        "-u",
+                        f"{receiver_user}",
+                        "-P",
+                        f"{receiver_pass}",
+                        "-q",
+                        "1",
+                        "-t",
+                        f"{__topic__}",
+                        "-m",
+                        f"{stats}",
+                    ]
+                ),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             mqttsend.check_returncode()
             logger.debug("Send statistics to MQTT broker successful.")
             success = True
         except subprocess.CalledProcessError as e:
             logger.error(
-                f"Send statistics to MQTT broker failed: Exit code {e.returncode}, {e.stderr.strip()}")
-            logger.error(
-                f"Exception: {e}")
+                f"Send statistics to MQTT broker failed: Exit code {e.returncode}, {e.stderr.strip()}"
+            )
+            logger.error(f"Exception: {e}")
             success = False
 
         return success
